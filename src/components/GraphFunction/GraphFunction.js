6d60e8f65465773d2fbc663e07a3f1dbf11523d5
endless_forest <ingrey1@gmail.com>
2023-09-08 23:55:28 -1000

    test canvas react

--- src/components/GraphFunction/GraphFunction.js
+++ src/components/GraphFunction/GraphFunction.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useEffect} from "react";
 
 const GraphFunction = () => {
-   return <div>
-      Function Graphing Coming Soon
-   </div>
-}
 
-export default GraphFunction
+  const canvas = <canvas id="graphing-canvas" width="500" height="500" style={{"border":"1px solid #000000"}}></canvas>  
+
+  useEffect(() => {
+    const canvas = document.getElementById("graphing-canvas");
+    const ctx = canvas.getContext("2d");
+
+    // Start a new Path
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(300, 150);
+    
+    // Draw the Path
+    ctx.stroke();
+  });
+
+ 
+
+  return (
+    <div>
+      {canvas}
+    </div>
+  );
+};
+
+export default GraphFunction;
