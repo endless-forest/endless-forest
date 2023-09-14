e178fcf0d12a1f8598c9b140866ee945d7bafd59
endless_forest <ingrey1@gmail.com>
2023-09-14 04:53:42 -0700

    updae star movement

--- src/components/MovableStar.js
+++ src/components/MovableStar.js
@@ -2,10 +2,24 @@ import React, { useEffect, useState } from "react";
 import { Button, Icon, Label } from "semantic-ui-react";
 import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 
-const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min);
+const incrementPoints = (points, setPoints, direction, velocity = 1) => {
+  
+    const newPoints = points.map((point) => {
+      const newPoint = {...point}
+      if (direction === "up") {
+        newPoint.y = newPoint.y + velocity;
+      } else if (direction === "down") {
+        newPoint.y = newPoint.y - velocity;
+      } else if (direction === "left") {
+        newPoint.x = newPoint.x - velocity;
+      } else {
+        newPoint.x = newPoint.x + velocity;
+      }
+      return newPoint
+    })
+
+    setPoints(newPoints)
+    return newPoints
 };
 
 const generatePolygon = (points) => {
@@ -19,7 +33,7 @@ const generatePolygon = (points) => {
       starExpression = starExpression + `(${point.x}, ${point.y}))`;
     }
   }
-  console.info("star expression", starExpression);
+  
   return {
     id: `movable-star`,
     latex: `${starExpression}`,
@@ -41,18 +55,27 @@ const createCalculator = () => {
   elt.style.width = "1200px";
   elt.style.height = "800px";
   elt.style.margin = "25px";
-  const calculator = Desmos.GraphingCalculator(elt, { expressions: false });
+  const calculator = Desmos.GraphingCalculator(elt, { expressions: false, settingsMenu: false});
   return calculator;
 };
 
-const updatePolygon = (e, calculator) => {
-  calculator.setBlank();
-  const expressions = [];
+const updatePolygon = (calculator, points) => {
+  const expressions = [generatePolygon(points)];
   graphExpressions(calculator, expressions);
 };
 
+const changeDirectionOnClick = (direction, velocity, points, calculator, setPoints) => {
+  const newPoints = incrementPoints(points, setPoints, direction, velocity)
+  updatePolygon(calculator, newPoints)
+
+  
+}
+
 const MovableStar = () => {
   const [calculator, setCalculator] = useState(null);
+  const [points, setPoints] = useState(null);
+  const [velocity, setVelocity] = useState(1);
+
   useEffect(() => {
     if (window) {
       const points = [
@@ -72,25 +95,26 @@ const MovableStar = () => {
       const expressions = [generatePolygon(points)];
       const newCalculator = createCalculator();
       setCalculator(newCalculator);
+      setPoints(points);
       graphExpressions(newCalculator, expressions);
     }
   }, []);
 
   return (
     <div>
-      <Label style={{margin: "20px"}} as='h2' color="violet" tag>
-      Use the keys to control the star's movement.
-    </Label>  
-      <Button icon color="teal">
+      <Label style={{ margin: "20px" }} as="h2" color="violet" tag>
+        Use the keys to control the star's movement.
+      </Label>
+      <Button icon color="teal" onClick={() => changeDirectionOnClick("left", velocity, points, calculator, setPoints)}>
         <Icon name="arrow left" />
       </Button>
-      <Button icon color="teal">
+      <Button icon color="teal" onClick={() => changeDirectionOnClick("right", velocity, points, calculator, setPoints)}>
         <Icon name="arrow right" />
       </Button>
-      <Button icon color="teal">
+      <Button icon color="teal" onClick={() => changeDirectionOnClick("up", velocity, points, calculator, setPoints)}>
         <Icon name="arrow up" />
       </Button>
-      <Button icon color="teal">
+      <Button icon color="teal" onClick={() => changeDirectionOnClick("down", velocity, points, calculator, setPoints)}>
         <Icon name="arrow down" />
       </Button>
       <div id="graph"></div>
