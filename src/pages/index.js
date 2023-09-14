a886d56c6368730db08e170eb3a674417118b4cb
endless_forest <ingrey1@gmail.com>
2023-09-13 21:18:02 -0700

    add circle timer

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+import 'semantic-ui-css/semantic.min.css'
 
 
 import styles from './index.module.css';
