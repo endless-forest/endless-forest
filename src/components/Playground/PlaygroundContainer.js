c9eb3933019c42bd58b913da00205474bbf8846c
endless_forest <ingrey1@gmail.com>
2024-03-27 07:55:33 -0700

    update images

--- src/components/Playground/PlaygroundContainer.js
+++ src/components/Playground/PlaygroundContainer.js
@@ -1,17 +1,11 @@
 import React from "react";
-import { math } from "@site/src/components/Playground/mathUtils.js";
 import foxImage from '@site/static/img/fox.png';
-import fortniteImage from '@site/static/img/brandon-fortnite-skin.png'
+
 
 const PlaygroundContainer = () => {
   return (
     <main>
       <div>
-        Using the mathJS library to represent negative square roots as i:
-        {" " + math.functions.sqrt(-4).toString()}
-      </div>
-      <div>
-        <img src={fortniteImage} />
         <img src={foxImage} />
       </div>
     </main>
