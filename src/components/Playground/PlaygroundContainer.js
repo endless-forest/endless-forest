cda1814038eb640807beb40fcabebb7e1b00431b
endless_forest <ingrey1@gmail.com>
2024-03-27 08:20:20 -0700

    update images

--- src/components/Playground/PlaygroundContainer.js
+++ src/components/Playground/PlaygroundContainer.js
@@ -1,12 +1,54 @@
 import React from "react";
-import foxImage from '@site/static/img/fox.png';
+import arcticFoxImage from '@site/static/img/arctic-fox.png';
+import arcticPoppyImage from '@site/static/img/arctic-poppy.png';
+import beaverImage from '@site/static/img/beaver.png';
+import bisonImage from '@site/static/img/bison.png';
+import cactusImage from '@site/static/img/cactus.png';
+import camelImage from '@site/static/img/camel.png';
+import deerImage from '@site/static/img/deer.png';
+import desertFoxImage from '@site/static/img/desert-fox.png';
+import desertLizardImage from '@site/static/img/desert-lizard.png';
+import evergreenImage from '@site/static/img/evergreen.png';
+import forestBearImage from '@site/static/img/forest-bear.png';
+import grasslandWolfImage from '@site/static/img/grassland-wolf.png';
+import oakTreeImage from '@site/static/img/oak-tree.png';
+import otterImage from '@site/static/img/otter.png';
+import penguinImage from '@site/static/img/penguin.png';
+import polarBearImage from '@site/static/img/polar-bear.png';
+import raccoonImage from '@site/static/img/raccoon.png';
+import rattleSnakeImage from '@site/static/img/rattle-snake.png';
+import snowyOwlImage from '@site/static/img/snowy-owl.png';
+import troutImage from '@site/static/img/trout.png';
+import turtleImage from '@site/static/img/turtle.png';
+import wolfImage from '@site/static/img/wolf.png';
+
 
 
 const PlaygroundContainer = () => {
   return (
     <main>
       <div>
-        <img src={foxImage} />
+        <img src={arcticFoxImage} />
+        <img src={beaverImage} />
+        <img src={bisonImage} />
+        <img src={cactusImage} />
+        <img src={camelImage} />
+        <img src={deerImage} />
+        <img src={desertFoxImage} />
+        <img src={desertLizardImage} />
+        <img src={evergreenImage} />
+        <img src={forestBearImage} />
+        <img src={grasslandWolfImage} />
+        <img src={oakTreeImage} />
+        <img src={otterImage} />
+        <img src={penguinImage} />
+        <img src={polarBearImage} />
+        <img src={raccoonImage} />
+        <img src={rattleSnakeImage} />
+        <img src={snowyOwlImage} />
+        <img src={troutImage} />
+        <img src={turtleImage} />
+        <img src={wolfImage} />
       </div>
     </main>
   );
